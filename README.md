d4662df400ac3eb3aec06a28500f322ab659bef2
Anit Gandhi <anitgandhi@gmail.com>
2016-01-29 14:48:20 -0600

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# ECE Pulse 2016 Software Workshp - Web Workers
+
+[Link to Slides](https://docs.google.com/presentation/d/1koMpz9e8ghGN1MR8LLO5JKQEepiN4UdiNZqRyl7fY6U/edit?usp=sharing)
+
+You are given three files in the `demo` folder: `index.html`, `index.js`, and `worker.js`. You will only need to modify `index.js` and `worker.js`. 
---
+++ demo/index.js
@@ -0,0 +1,25 @@
+var data = {
+	// A labels array that can contain any sort of values
+	labels: [' ', ' ', ' ', ' ', ' '],
+	// Our series array that contains series objects or in this case series data arrays of heartbeat values
+	series: [
+		[72, 70, 68, 72, 88]
+	]
+};
+
+// Create a new line chart object where as first parameter we pass in a selector
+// that is resolving to our chart container element. The Second parameter
+// is the actual data object.
+var myChart = new Chartist.Line('.ct-chart', data);
+
+function update() {
+	var currWidth	=	$(".chart-container").width();
+    var newWidth	=	currWidth + 40;
+  	$(".chart-container").width(newWidth);
+  	var rightMost = $('.chart-container').width();
+	$('.screen').scrollLeft(rightMost);
+}
+
+$(document).ready(function(){
+	// FILL IN YOUR CODE HERE
+});
